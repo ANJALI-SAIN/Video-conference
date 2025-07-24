d12a7e91ea9389801077b01e39a5fa0a2b281425
ANJALI-SAIN <anjalipersonal19@gmail.com>
2025-07-24 18:16:27 +0530

    Final commit - complete Zoom project with frontend and backend

---
+++ frontend/src/App.js
@@ -0,0 +1,27 @@
+
+import './App.css';
+import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
+import LandingPage from './pages/landing';
+import Authentication from './pages/authentication';
+import { AuthProvider } from './contexts/AuthContext';
+import VideoMeetComponent from './pages/VideoMeet';
+function App() {
+  return (
+   <>
+   <Router>
+    <AuthProvider>
+
+    
+    <Routes>
+      <Route path="/" element={<LandingPage/>}></Route>
+      <Route path="/auth" element={<Authentication/>}></Route>
+     <Route path="/:url" element={<VideoMeetComponent />} />
+
+    </Routes>
+    </AuthProvider>
+   </Router>
+   </>
+  );
+}
+
+export default App;
